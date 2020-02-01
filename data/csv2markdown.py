f9ba54444c0dc37b9226d660fc08e9d8619e7742
Weilei Zeng <240155787@qq.com>
2020-02-01 09:33:48 -0800

    add data

--- data/csv2markdown.py
+++ data/csv2markdown.py
@@ -25,33 +25,72 @@ def convert(csv_file_path, html_file_path,table_name):
     writer.from_csv(csv_file_path)
     writer.table_name = table_name
 
-    #limit width of first colum to be
+
+    #get the column for link
+    link_col=0
+    for head in writer.headers:
+        #print(head)
+        if ( head == '链接' ):
+            break
+        else:
+            link_col +=1
+    #debug
+    if ( link_col >= len(writer.headers) ):
+        raise ValueError('no column for link')
+    
+    print('get link_col = ',link_col)
+
+    #get the column for wuzi
+    wuzi_col=0
+    for head in writer.headers:
+        #print(head)
+        if ( head == '物资' ):
+            break
+        else:
+            wuzi_col +=1
+    #debug
+    wuzi_col_flag = True
+    if ( wuzi_col >= len(writer.headers) ):
+        wuzi_col_flag = False
+        #raise ValueError('no column for wuzi')
+    
+    print('get wuzi_col = ',wuzi_col)
+
+    
+    #limit width of some column to be
+    col_fixed_width=wuzi_col
     col_width=24
     #modify the link in the fourth row to be an html link
+    
     for row in writer.value_matrix:
-        if len(row[0]) > col_width :
-            row[0]=row[0][0:col_width]
+        if wuzi_col_flag and len(row[col_fixed_width]) > col_width :
+            row[col_fixed_width]=row[col_fixed_width][0:col_width]
         #row[3]='\n <a href="'+row[3]+'"> link </a> \n'
-        row[3]='[link]('+row[3]+')'
-    #print('tag ->',writer.is_escape_html_tag)
-    #writer.is_escape_html_tag=True
-    #print('tag ->',writer.is_escape_html_tag)
+        if ( row[link_col] != '' ):
+            row[link_col]='[link]('+row[link_col]+')'
     writer.dump(html_file_path)
-    #writer.write_table()
     print("done with ",table_name)
 
 def main():
+    #convert all csv to markdown
     data_path=""
     build_path="./../page/"
+    
     csv_file_path = "inflow.csv"
     html_file_path = "inflow.md"
     table_name = '' #"物资流入表"
     convert(data_path + csv_file_path, build_path + html_file_path,table_name)
+    
     csv_file_path = "outflow.csv"
     html_file_path = "outflow.md"
     table_name = '' #"物资流出表"
     convert(data_path + csv_file_path, build_path + html_file_path,table_name)
-    
+
+    csv_file_path = "donate.csv"
+    html_file_path = "donate.md"
+    table_name = '' 
+    convert(data_path + csv_file_path, build_path + html_file_path,table_name)
+
 if __name__=="__main__":
 
     main()
--- merge_files.sh
+++ merge_files.sh
@@ -5,11 +5,18 @@ cd page
 echo "this script merge all parts of readme into one file $out"
 
 cat head.md > $out
-echo "物资流入表" >> $out
+
+echo "### 累计捐赠" >> $out
 echo >> $out
-cat inflow.md >> $out
+cat donate.md >> $out
 echo >> $out
-echo "物资流出表" >> $out
+
+#echo "### 物资流入表" >> $out
+#echo >> $out
+#cat inflow.md >> $out
+#echo >> $out
+
+echo "### 物资流出表" >> $out
 echo >> $out
 cat outflow.md >> $out
 echo >> $out
