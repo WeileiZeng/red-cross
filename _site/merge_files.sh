cf1d548355a111af0e089092aaff02289012a324
Weilei Zeng <240155787@qq.com>
2020-02-01 10:12:39 -0800

    add data for donation

--- _site/merge_files.sh
+++ _site/merge_files.sh
@@ -6,7 +6,7 @@ echo "this script merge all parts of readme into one file $out"
 
 cat head.md > $out
 
-echo "### 累计捐赠" >> $out
+echo "### [湖北省红十字会](http://hbsredcross.org.cn/)累计捐赠情况" >> $out
 echo >> $out
 cat donate.md >> $out
 echo >> $out
@@ -16,7 +16,7 @@ echo >> $out
 #cat inflow.md >> $out
 #echo >> $out
 
-echo "### 物资流出表" >> $out
+echo "### [1月30日公布物资使用情况](http://www.hbsredcross.org.cn/xxgk/4704.jhtml)" >> $out
 echo >> $out
 cat outflow.md >> $out
 echo >> $out
---
+++ raw_data/readme.md
@@ -0,0 +1,6 @@
+This folder saves some raw data, including tables downloaded from the source links
+
+
+note:
+
+donate04 is not posted online
